b82a90a9c6908dbdc3be4a9d4a121599543ccb33
kingmats12 <kingmatems@gmail.com>
2025-05-01 16:46:46 +0200

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "nector~I9oUgKyJ#kOuae8ng6czmJ-SETB4PvcaOIzi5pTNwiH4xn73idLA",
+  SESSION_ID: process.env.SESSION_ID || "nector~d55XEAKY#MGQBH2spCS8INKOQTv5Ycw_nhoPqWiEILE0KUEWcGIQ",
   PREFIX: process.env.PREFIX || '*',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY !== undefined ? process.env.AUTO_STATUS_REPLY === 'true' : true,
